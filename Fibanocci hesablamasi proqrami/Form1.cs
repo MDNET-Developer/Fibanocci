a20d87928c85994c6f0ea9dd425e8558b577b8c4
MDNET-Developer <qarabagli.murad@mail.ru>
2022-10-06 23:39:31 +0400

    new app

---
+++ Fibanocci hesablamasi proqrami/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fibanocci_hesablamasi_proqrami
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double say2 = 1;
+            double say1 = 1;
+            double say3;
+
+            double n;
+            n = double.Parse(textBox1.Text);
+
+           
+
+            for(double i = 1; i <= n; i++)
+            {
+                listBox1.Items.Add(say1);
+                listBox1.Items.Add(say2);
+                say3 = say1 + say2;
+                say1 = say2;
+                say2 = say3;
+                listBox1.Items.Add(say3);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            textBox1.Clear(); 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Cursor = Cursors.WaitCursor;
+            MessageBox.Show(" The program was closed !!! ");
+            Application.Exit();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form2 izahat = new Form2();
+            izahat.Show();
+            this.Hide();
+            
+        }
+    }
+}
---
+++ Fibanocci hesablamasi proqrami/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fibanocci_hesablamasi_proqrami
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = " Cədvəldə oxlarla göstərildiyi \r\n" +
+                          "kimi say2 uygun olaraq say1 -ə\r\n" +
+                          "say3 uyğun olaraq say2-ə keçir\r\n" +
+                          "Bu cür yer dəyişmələr ilə uyğun olaraq\r\n" +
+                          "ardicil qaydada bir-biriləri ilə toplanirlar\r\n" +
+                          "Ona görədə alqoritmada say1=say2 və say2=say3\r\n" +
+                          "yazdiq\r\n" +
+                          "\r\n" +
+                          "\r\n" +
+                          "\r\n" +
+                          "\r\n" +
+                          "1+1=2\r\n" +
+                          "1+2=3\r\n" +
+                          "2+3=5\r\n" +
+                          "3+5=8\r\n" +
+                          "5+8=13\r\n" +
+                          "8+13=21\r\n" +
+                          "Sürüşdürmə qaydası baş verdi";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 geri = new Form1();
+            geri.Show();
+            this.Hide();
+        }
+    }
+}
